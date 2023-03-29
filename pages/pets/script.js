b9a883588a0a56646ebbc57a3613af9e11fa68aa
lyutails <lyusya.cool@gmail.com>
2023-03-29 06:00:27 +0300

    fix: make both link and icon in footer to be the link

--- pages/pets/script.js
+++ pages/pets/script.js
@@ -1,2 +1,5 @@
-const paginationDisabledButtons = document.querySelector('#disabled')
-aginationDisabledButtons.disabled = true;
+const paginationDisabledButtonOne = document.querySelector('#disabled_one')
+aginationDisabledButtonOne.disabled = true;
+
+const paginationDisabledButtonTwo = document.querySelector('#disabled_two')
+aginationDisabledButtonTwo.disabled = true;
