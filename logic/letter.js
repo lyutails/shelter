f542059c3c1f127762708bcf3920d308d05d50bb
lyutails <lyusya.cool@gmail.com>
2024-05-28 07:22:07 +0300

    feat: make letter return by hover again

--- logic/letter.js
+++ logic/letter.js
@@ -1,25 +1,32 @@
 export function runningLetter() {
   console.log("letter1");
-  const client = document.querySelector('body');
+  const client = document.querySelector("body");
   const clientRect = client.getBoundingClientRect();
   let clientX = clientRect.left;
   let clientY = clientRect.top;
   const logoWrapper = document.querySelector(".logo_wrapper");
   const movingLetter = document.querySelector(".logo_icon");
-  logoWrapper.addEventListener("mousemove", () => {
-    document.body.onpointermove = (event) => {
-      const { clientX, clientY } = event;
+  logoWrapper.addEventListener("mouseover", () => {
+    if ((movingLetter.style.position != "fixed")) {
+      movingLetter.style.position = "fixed";
+      movingLetter.style.marginLeft = '80px';
+      document.body.onpointermove = (event) => {
+        const { clientX, clientY } = event;
 
-      movingLetter.animate(
-        [
-          {
-            left: `${clientX}px`,
-            top: `${clientY}px`,
-          },
-        ],
-        { duration: 1000, iterations: Infinity }
-      );
-    };
+        movingLetter.animate(
+          [
+            {
+              left: `${clientX}px`,
+              top: `${clientY}px`,
+            },
+          ],
+          { duration: 1000, iterations: Infinity }
+        );
+      };
+    } else {
+      movingLetter.style.position = "unset";
+      movingLetter.style.marginLeft = '0px';
+    }
   });
   return logoWrapper;
 }
